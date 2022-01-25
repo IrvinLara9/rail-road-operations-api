bcceb7d5cf5d4a016bd30a80294301ee53dbc928
“Irvin <irvinlaravillalobos@yahoo.com.mx>
2022-01-25 10:31:03 -0700

    Add database init.sh

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Drop
+aws dynamodb delete-table --endpoint http://localhost:8000 --table-name Train
+
+
+# Create
+aws dynamodb create-table --endpoint http://localhost:8000 --table-name Train \
+  --attribute-definitions AttributeName=type,AttributeType=S AttributeName=name,AttributeType=S \
+    --key-schema AttributeName=type,KeyType=HASH AttributeName=name,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+# Insert
+aws dynamodb put-item --endpoint http://localhost:8000  --table-name Train \
+    --item '{"type":{"S":"Destination"}, "name": {"S": "Chicago"}}'
+
+aws dynamodb put-item --endpoint http://localhost:8000  --table-name Train \
+--item '{"type":{"S":"Destination"}, "name": {"S": "Houston"}}'
+
+aws dynamodb put-item --endpoint http://localhost:8000  --table-name Train \
+--item '{"type":{"S":"Destination"}, "name": {"S": "LA"}}'
+
+# Disply all values in table
+aws dynamodb scan --endpoint http://localhost:8000  --table-name Train
